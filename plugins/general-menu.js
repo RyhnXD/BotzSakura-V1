57320fde93cd6d1b60ecdb767c98cc972b9715ea
Kaguya Shinomiya <102699105+RyhnXD@users.noreply.github.com>
2022-07-16 19:00:29 +0700

    Update general-menu.js

--- plugins/general-menu.js
+++ plugins/general-menu.js
@@ -1,134 +1,52 @@
-import { promises } from 'fs'
-import { join } from 'path'
-import { xpRange } from '../lib/levelling.js'
-import Canvas from "discord-canvas"
+//Make & Help By
+//Johannes & Papah-Chan
+import jimp from 'jimp'
+import fs from 'fs'
+import fetch from 'node-fetch'
+import PhoneNumber from 'awesome-phonenumber'
+import moment from 'moment-timezone'
 
-let tags = {
-  'main': 'Main',
-  'rpg': 'RolePlay Games',
-  'xp': 'Exp & Limit',
-  'jadian': 'Jadian',
-  'sticker': 'Sticker',
-  'edukasi': 'Edukasi',
-  'quran': 'Al Quran',
-  'tools': 'Tools',
-  'kerang': 'Kerang Ajaib',
-  'primbon': 'Primbon',
-  'fun': 'Fun',
-  'game': 'Game',
-  'quotes': 'Quotes',
-  'audio': 'Audio',
-  'maker': 'Maker',
-  'downloader': 'Downloader',
-  'internet': 'Internet',
-  'random': 'Random',
-  'nsfw': 'Nsfw',
-  'nulis': 'MagerNulis & Logo',
-  'anonymous': 'Anonymous Chat',
-  'database': 'Database',
-  'admin': 'Admin',
-  'group': 'Group',
-  'vote': 'Voting',
-  'absen': 'Absen',
-  'premium': 'Premium',
-  'advanced': 'Advanced',
-  'info': 'Info',
-  'owner': 'Owner',
-  'jadibot': 'Jadi Bot',
-  'host': 'Host',
-  'Baileys': 'Baileys',
-  'nocategory': 'No Category',
-}
-let emot = `${pickRandom(['⎔', '✦', '⭑', 'ᯬ', '⭔', '◉', '⬟', '▢', '᭻', '»', '〆', '々', '⛥', '✗', '⛊', '⚜', '⚝', '⚚', '♪'])}`
+let tags = {}
 const defaultMenu = {
-  before: `
-Hai, *%name!*
-
-*Tanggal:* %week, %date
-*Waktu:* %time
-*Bot Online:* %uptime (%muptime)
-*Pengguna:* %totalreg Orang
-*Lib:* Baileys-Md
-*Language:* Javascript,Ts-Node
-*Fitur:* %totalfeatures command
-
-%readmore
-*Note:*
-_Jika Respon Tidak Muncul Kemungkinan Terjadi Error_
-`.trimStart(),
-  header: `${cmenut} *%category* ${cmenuh}`,
-  body: `┊${emot} %cmd %islimit %isPremium`,
-  footer: `${cmenuf}`,
-  after: `${cmenua}`,
+  before: `\n> Date: %date\n> Time: %time \n> Runtime: %uptime\n%readmore`,
+  header: '*❏═┅═━–〈 %category*',
+  body: '┊› %cmd %islimit %isPremium',
+  footer: '',
+  after: '',
 }
-let handler = async (m, { conn, groupMetadata, usedPrefix: _p, __dirname }) => {
+
+let handler = async (m, { conn, usedPrefix: _p }) => {
   try {
-    let _package = JSON.parse(await promises.readFile(join(__dirname, '../package.json')).catch(_ => ({}))) || {}
-    let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-    let { exp, limit, level, role, money, lastclaim, lastweekly, registered, regTime, age, banned, pasangan } = global.db.data.users[who]
-    let { min, xp, max } = xpRange(level, global.multiplier)
-    let name = await conn.getName(who)
-    let pp = await conn.profilePictureUrl(who).catch(_ => './src/avatar_contact.png')
-    if (typeof global.db.data.users[who] == "undefined") {
-      global.db.data.users[who] = {
-        exp: 0,
-        limit: 10,
-        lastclaim: 0,
-        registered: false,
-        name: conn.getName(m.sender),
-        age: -1,
-        regTime: -1,
-        afk: -1,
-        afkReason: '',
-        banned: false,
-        level: 0,
-        lastweekly: 0,
-        role: 'Warrior V',
-        autolevelup: false,
-        money: 0,
-        pasangan: "",
-      }
-     }
-     let math = max - xp
-    let totalfeatures = Object.values(global.plugins).filter(
-    (v) => v.help && v.tags
-  ).length;
+    let name = m.pushName || conn.getName(m.sender)
     let d = new Date(new Date + 3600000)
-    let locale = 'id'
+    let locale = 'en'
     // d.getTimeZoneOffset()
     // Offset -420 is 18.00
     // Offset    0 is  0.00
     // Offset  420 is  7.00
-    let weton = ['Pahing', 'Pon', 'Wage', 'Kliwon', 'Legi'][Math.floor(d / 84600000) % 5]
-    let week = d.toLocaleDateString(locale, { weekday: 'long' })
     let date = d.toLocaleDateString(locale, {
       day: 'numeric',
       month: 'long',
-      year: 'numeric'
+      year: 'numeric',
+      timeZone: 'Asia/Jakarta'
     })
-    let dateIslamic = Intl.DateTimeFormat(locale + '-TN-u-ca-islamic', {
-      day: 'numeric',
-      month: 'long',
-      year: 'numeric'
-    }).format(d)
-    let time = d.toLocaleTimeString(locale, {
-      hour: 'numeric',
-      minute: 'numeric',
-      second: 'numeric'
-    })
-    let _uptime = process.uptime() * 1000
-    let _muptime
+    let time = d.toLocaleTimeString(locale, { timeZone: 'Asia/Jakarta' })
+    time = time.replace(/[.]/g, ':')
+    let _uptime
     if (process.send) {
       process.send('uptime')
-      _muptime = await new Promise(resolve => {
+      _uptime = await new Promise(resolve => {
         process.once('message', resolve)
         setTimeout(resolve, 1000)
       }) * 1000
     }
-    let muptime = clockString(_muptime)
+    let res = await fetch('https://raw.githubusercontent.com/ArugaZ/grabbed-results/main/random/anime/neko.txt')
+    let txt = await res.text()
+    let arr = txt.split('\n')
+    let cita = arr[Math.floor(Math.random() * arr.length)]
+    let thumb = await(await fetch(cita)).buffer()
+      let vn = './media/bot.mp3'
     let uptime = clockString(_uptime)
-    let totalreg = Object.keys(global.db.data.users).length
-    let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length
     let help = Object.values(global.plugins).filter(plugin => !plugin.disabled).map(plugin => {
       return {
         help: Array.isArray(plugin.tags) ? plugin.help : [plugin.help],
@@ -148,16 +66,16 @@ let handler = async (m, { conn, groupMetadata, usedPrefix: _p, __dirname }) => {
     let header = conn.menu.header || defaultMenu.header
     let body = conn.menu.body || defaultMenu.body
     let footer = conn.menu.footer || defaultMenu.footer
-    let after = conn.menu.after || (conn.user.jid == global.conn.user.jid ? '' : `Powered by https://wa.me/${global.conn.user.jid.split`@`[0]}`) + defaultMenu.after
+    let after = conn.menu.after || defaultMenu.after
     let _text = [
       before,
       ...Object.keys(tags).map(tag => {
-        return header.replace(/%category/g, tags[tag]) + '\n' + [
+        return header.replace(/%category/g, tags[tag].toUpperCase()) + '\n' + [
           ...help.filter(menu => menu.tags && menu.tags.includes(tag) && menu.help).map(menu => {
             return menu.help.map(help => {
               return body.replace(/%cmd/g, menu.prefix ? help : '%p' + help)
-                .replace(/%islimit/g, menu.limit ? '(Limit)' : '')
-                .replace(/%isPremium/g, menu.premium ? '(Premium)' : '')
+                .replace(/%islimit/g, menu.limit ? 'Ⓛ' : '')
+                .replace(/%isPremium/g, menu.premium ? 'Ⓟ' : '')
                 .trim()
             }).join('\n')
           }),
@@ -169,35 +87,46 @@ let handler = async (m, { conn, groupMetadata, usedPrefix: _p, __dirname }) => {
     let text = typeof conn.menu == 'string' ? conn.menu : typeof conn.menu == 'object' ? _text : ''
     let replace = {
       '%': '%',
-      p: _p, uptime, muptime,
+      p: _p, uptime,
       me: conn.getName(conn.user.jid),
-      npmname: _package.name,
-      npmdesc: _package.description,
-      version: _package.version,
-      exp: exp - min,
-      maxexp: xp,
-      totalexp: exp,
-      xp4levelup: max - exp,
-      github: _package.homepage ? _package.homepage.url || _package.homepage : '[unknown github url]',
-      level, totalfeatures, limit, name, weton, week, date, dateIslamic, time, totalreg, rtotalreg, role,
+      name, date, time,
       readmore: readMore
     }
+    let fkon = { key: { fromMe: false, participant: `${m.sender.split`@`[0]}@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
-    
-    conn.sendHydrated(m.chat, text.trim(), wm + '\n\n' + botdate, hwaifu.getRandom(), gcwangsaf, 'Sakura Group', who.split`@`[0], 'Your Number', [
-      ['🎀 Menu', '/menu'],
-      ['🪄 Owner', '/owner'],
-      ['🔖 Test', '/ping']
-    ], null, false, { mentions: [text] })
-    
+    // const pp = await conn.profilePictureUrl(conn.user.jid, 'image').catch(_ => './src/avatar_contact.png')
+    // if (m.isGroup) return conn.sendButton(m.chat, text.trim(), conn.getName(conn.user.jid), pp, [['Speedtest', _p + 'ping'], ['Owner', _p + 'owner']], m)
+    //conn.sendHydrated(m.chat, text.trim(), conn.getName(conn.user.jid), await genProfile(conn, m), 'https://youtube.com/channel/UC0hs_I8N3JntK5vO6KogavQ', 'YouTube', null, null, [['Speedtest', _p + 'ping'], ['Owner', _p + 'owner']], m)
+   // conn.sendMessage(m.chat, { video: { url: 'https://telegra.ph/file/c82d5c358495e8ef15916.mp4' }, gifPlayback: true, gifAttribution: ~~(Math.random() * 2), caption: text.trim(), footer: await conn.getName(conn.user.jid) , templateButtons: [{ quickReplyButton: { displayText: 'Speedtest', id: `${_p}ping` }}, { quickReplyButton: { displayText: 'Owner', id: `${_p}owner` }} ] })
+   conn.sendButton(m.chat, `*${wish()}, ${name} 👋*`, text.trim(), await genProfile(conn, m), [['Speedtest', _p + 'ping'], ['Owner', _p + 'owner']], false, { quoted: fkon, contextInfo: { externalAdReply: { showAdAttribution: true,
+    mediaUrl: "https://chat.whatsapp.com/KOughnpmtzzF0k9aX78CVq",
+    mediaType: "FOTO",
+    description: "https://Instagram.com/bot_whangsaf", 
+    title: 'Sakura Esm',
+    body: wm,
+    thumbnail: thumb,
+    sourceUrl: sgc
+}
+} })
+conn.sendFile(m.chat, vn, 'bot.mp3', null, m, true, {
+type: 'audioMessage', 
+ptt: true 
+})
+    // conn.sendButton(m.chat, 
+    //`*Hi, ${name} 👋*\n\n`, 
+  //  text.trim(), './media/marin.jpg', [
+// [`Speedtest`, `${_p}ping`],
+// [`Owner`, `${_p}owner`]
+//], m, {asLocation: true})
   } catch (e) {
-    conn.reply(m.chat, 'Maaf, menu sedang error', m)
+    m.reply('An error occurred')
     throw e
   }
 }
-
-handler.command = /^(.m|help|\?)$/i
-
+handler.help = ['m']
+handler.tags = ['general']
+handler.alias = ['m']
+handler.command = /^(m)$/i
 handler.exp = 3
 
 export default handler
@@ -212,6 +141,45 @@ function clockString(ms) {
   return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
 }
 
-function pickRandom(list) {
-     return list[Math.floor(Math.random() * list.length)]
+function wish() {
+    let wishloc = ''
+  const time = moment.tz('Asia/Kolkata').format('HH')
+  wishloc = ('Hi')
+  if (time >= 0) {
+    wishloc = ('Night Rider')
   }
+  if (time >= 4) {
+    wishloc = ('Good Morning')
+  }
+  if (time >= 12) {
+    wishloc = ('Good Afternoon')
+  }
+  if (time >= 16) {
+    wishloc = ('️Good Evening')
+  }
+  if (time >= 23) {
+    wishloc = ('Night Rider')
+  }
+  return wishloc
+}
+
+async function genProfile(conn, m) {
+  let font = await jimp.loadFont('./names.fnt'),
+    mask = await jimp.read('https://i.imgur.com/552kzaW.png'),
+    welcome = await jimp.read(thumbnailUrl.getRandom()),
+    avatar = await jimp.read(await conn.profilePictureUrl(m.sender, 'image').catch(() => 'https://telegra.ph/file/24fa902ead26340f3df2c.png')),
+    status = (await conn.fetchStatus(m.sender).catch(console.log) || {}).status?.slice(0, 30) || 'Not Detected'
+
+    await avatar.resize(460, 460)
+    await mask.resize(460, 460)
+    await avatar.mask(mask)
+    await welcome.resize(welcome.getWidth(), welcome.getHeight())
+
+    await welcome.print(font, 550, 180, 'Name:')
+    await welcome.print(font, 650, 255, m.pushName.slice(0, 25))
+    await welcome.print(font, 550, 340, 'About:')
+    await welcome.print(font, 650, 415, status)
+    await welcome.print(font, 550, 500, 'Number:')
+    await welcome.print(font, 650, 575, PhoneNumber('+' + m.sender.split('@')[0]).getNumber('international'))
+    return await welcome.composite(avatar, 50, 170).getBufferAsync('image/png')
+}
